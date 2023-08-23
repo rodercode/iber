cff9a13c72ad9e3df1e246a3715a6b515f351316
rodercode <rodersnow@gmail.com>
2023-08-23 13:34:17 +0200

    Initial commit

---
+++ src/main/java/com/example/iber/IberApplication.java
@@ -0,0 +1,13 @@
+package com.example.iber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iber/IberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
