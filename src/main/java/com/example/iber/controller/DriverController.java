e190e03fb9016ac4876e1471e463c17dbf38aca4
RoderCode <Rodersnow@gmail.com>
2023-08-23 18:45:33 +0200

    replace autowired annotations with a constructure injection

--- src/main/java/com/example/iber/controller/DriverController.java
+++ src/main/java/com/example/iber/controller/DriverController.java
@@ -1,15 +1,20 @@
 package com.example.iber.controller;
 import com.example.iber.model.Driver;
 import com.example.iber.service.DriverService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @RestController
+@RequestMapping("api/drivers")
 public class DriverController {
-    @Autowired
-    private DriverService driverService;
-    @GetMapping("/drivers")
+
+    private final DriverService driverService;
+
+    public DriverController(DriverService driverService) {
+        this.driverService = driverService;
+    }
+    @GetMapping
     List<Driver> getAll() {
         return driverService.getAllDrivers();
     }
