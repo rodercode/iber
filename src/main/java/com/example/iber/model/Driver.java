ec728e86593e7ae4dcd8ba2388acfc0b111bba79
rodercode <rodersnow@gmail.com>
2023-08-23 22:07:57 +0200

    add Column annotation that prevent a field to have value null

--- src/main/java/com/example/iber/model/Driver.java
+++ src/main/java/com/example/iber/model/Driver.java
@@ -1,22 +1,32 @@
 package com.example.iber.model;
 import jakarta.persistence.*;
 import lombok.*;
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
+
+@Entity(name = "driver")
 public class Driver {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    @Column()
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "rating", nullable = false)
     private int rating;
+
+    @Column(name = "car", nullable = false)
     private String car;
-    @Column(name = "carLicense")
+
+    @Column(name = "carLicense", nullable = false)
     private String carLicense;
+
+    @Column(name = "location", nullable = false)
     private String location;
-    @Column(name = "passengerCapacity")
+
+    @Column(name = "passengerCapacity", nullable = false)
     private int passengerCapacity;
+
 }
